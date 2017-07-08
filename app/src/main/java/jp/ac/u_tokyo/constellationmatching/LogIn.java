f91183f46faf286590651330ca3ba591eef3282a
akg <akg@akg-no-MacBook-Pro-2.local>
2017-07-08 14:32:52 +0900

    First commit

---
+++ app/src/main/java/jp/ac/u_tokyo/constellationmatching/LogIn.java
@@ -0,0 +1,13 @@
+package jp.ac.u_tokyo.constellationmatching;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Friend);
+    }
+}
